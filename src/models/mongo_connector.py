90bd8adb9869c0969cb4541cf9326384380f894a
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2021-12-14 18:28:12 +0100

    Chgmt mongo certif pour usage perso

--- src/models/mongo_connector.py
+++ src/models/mongo_connector.py
@@ -21,7 +21,7 @@ class MongoConnector:
     """
 
     def __init__(self):
-        certificat_path = config.ROOT_DIR + "/certif_mongo.pem"
+        certificat_path = config.ROOT_DIR + "/2TM1-G3.pem"
         uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
         client = MongoClient(uri,
                              tls=True,
