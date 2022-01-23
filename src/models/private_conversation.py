b27c2281c596bc43a28e26e2dfa7cd12c8a13602
louislombaert <71373079+LouisLombaert@users.noreply.github.com>
2022-01-23 11:41:59 +0100

    ajout de docstrings

--- src/models/private_conversation.py
+++ src/models/private_conversation.py
@@ -5,6 +5,11 @@ from src.models.mongo_connector import MongoConnector
 
 class PrivateConversation:
     def __init__(self, id, name: str, members: list, last_message=None):
+        """init a new private conversation"""
+        """
+        PRE : name is a str, members is a list of str
+        POST : a new private conversation is created
+        """
         if id is None:
             self._id = uuid.uuid4()
         else:
@@ -25,6 +30,7 @@ class PrivateConversation:
         return self._id
 
     def send_conversation_on_db(self):
+        """send the conversation to the db"""
         query = {'_id': self._id,
                  'name': self.name,
                  'members': self.members,
@@ -33,6 +39,11 @@ class PrivateConversation:
         self.__collection.insert_one(query)
 
     def add_member(self, new_member):
+        """add 'new_member' to the members list"""
+        """
+        PRE : new_member is a string
+        POST : new_member is added to the list 'members'
+        """
         # ajout dans l'objet
         self.members.append(new_member)
         # ajout dans la DB
@@ -46,6 +57,11 @@ class PrivateConversation:
         pass
 
     def update_last_message(self, new_timestamp):
+        """update the last message of the conversation"""
+        """
+        PRE : new_timestamp is a datetime object
+        POST : the last message is changed
+        """
         self.last_message = new_timestamp
         query = {"_id": self._id}
         new_last_message = {"$set": {
@@ -54,6 +70,7 @@ class PrivateConversation:
         self.__collection.update_one(filter=query, update=new_last_message)
 
     def get_messages_from_db(self):
+        """init the messages from the db"""
         self.messages = []
         try:
             with MongoConnector() as connector:
@@ -66,4 +83,5 @@ class PrivateConversation:
             print(error)
 
     def update_messages_from_db(self):
+        """init the messages from the db"""
         self.get_messages_from_db()
